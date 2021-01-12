92ad58752d1c77e5a129c1a10be27c8ae468dfe9
samir-elhelw76 <elhelwsamir@gmail.com>
2021-01-11 22:31:34 -0500

    adding powershell script to pull sysinternals tools

---
+++ GetSysinternalsTools.ps1
@@ -0,0 +1,9 @@
+﻿$uri = "live.sysinternals.com"
+$tools = @("procexp64.exe", "Autoruns64.exe", "Tcpview.exe", "sigcheck64.exe", "Procmon64.exe", "Sysmon64.exe")
+$dirname = "sysinternals"
+
+mkdir $dirname
+
+foreach($tool in $tools) {
+    Invoke-WebRequest -uri "$uri/$tool" -OutFile "$dirname\$tool"
+}
