787fe860f54c48fa80ce3bb57f306b30e0a6c213
Matthew Kline <31708500+MatthewFKline@users.noreply.github.com>
2021-01-12 21:44:02 -0500

    Fixed Password Change Typos

--- Mass_Change_Domain_Passwords.ps1
+++ Mass_Change_Domain_Passwords.ps1
@@ -2,5 +2,5 @@ Import-Module ActiveDirectory
 $set = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray()
 $allusers = Get-ADUser -Properties memberof -Filter *
 $non_DA_users = foreach ($user in $allusers) {if ($user.MemberOf -join ';' -notmatch 'Domain Admins' -and $user.name -notmatch 'krbtgt') {Write-Output $user.name}}
-foreach ($user in $non_DA_users) {$password="";for ($i=1; $i -le 64; $i++){password+=$set | Get-Random};net user $user $password}
+foreach ($user in $non_DA_users) {$password="";for ($i=1; $i -le 64; $i++){$password += $set | Get-Random};net user $user $password}
 
